9aa3e656bb794fe4d31c801c421b0d96c459de2e
Heather McNeil <hd_mcneil@yahoo.com>
2023-09-07 16:38:32 -0700

    Add testing for health and sunk? method

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,10 +2,18 @@ require './lib/ship'
 
 RSpec.describe Ship do
   before :each do
-    cruiser = Ship.new("Cruiser", 3) 
+    @cruiser = Ship.new("Cruiser", 3) 
   end
 
   it 'is instance object of SHIP' do
-    expect(cruiser = Ship.new("Cruiser", 3) ).to be_an_instance_of(Ship)
+    expect(@cruiser = Ship.new("Cruiser", 3) ).to be_an_instance_of(Ship)
   end
+
+  it 'has atributes' do
+    expect(@cruiser.name).to eq("Cruiser")
+    expect(@cruiser.length).to eq(3)
+    expect(@cruiser.health).to eq(3)
+    expect(@cruiser.sunk?).to eq(false)
+  end
+
 end
