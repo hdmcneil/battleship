ef05fc6d7b2dce22292193a1ffd7c6e40cf29e5b
Heather McNeil <hd_mcneil@yahoo.com>
2023-09-10 13:01:50 -0700

    Add GameSetup class and ComputerPlayer class

---
+++ spec/computer_player_spec.rb
@@ -0,0 +1,32 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game_setup'
+require './lib/computer_player'
+require 'pry'
+
+RSpec.describe Board do
+  before :each do
+    @computer = ComputerPlayer.new
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 3)
+  end
+
+  it 'is an instance of ComputerPlayer class' do
+    expect(@computer).to be_an_instance_of(ComputerPlayer)
+  end
+
+  it 'can place a cruiser' do
+    @computer.place_cruiser(@board)
+    expect(@cruiser.length).to eq(3)
+    binding.pry
+    
+  end
+
+  it 'can place a submarine' do
+    @computer.place_submarine(@board)
+    expect(@submarine.length).to eq(2)
+    binding.pry
+  end
+end
