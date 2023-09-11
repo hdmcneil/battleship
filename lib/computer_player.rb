30b4dad2979031c073ff46d401d46ba3a0f56cfe
Blaine Kennedy <blainekennedy@gmail.com>
2023-09-11 16:00:44 -0500

    feat: add classes computer_player, player, and game

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,67 +1,54 @@
 require 'pry'
-
 class ComputerPlayer
   attr_reader
-
   def initialize
-
+    @computer_ships = []
   end
-
   def place_ships(board)
     place_cruiser(board)
     place_submarine(board)
   end
-
   def place_cruiser(board)
     cruiser = Ship.new("Cruiser", 3)
     valid_placement = false
-
     until valid_placement
       coordinates = generate_random_coordinates(cruiser.length)
       valid_placement = board.valid_placement?(cruiser, coordinates)
     end
-    
-    board.place(cruiser, coordinates)
-    
+    omputer_board.place(cruiser, coordinates)
+    computer_ships << cruiser
+    @pcomputer_board.display
   end
-
   def place_submarine(board)
     submarine = Ship.new("Submarine", 2)
     valid_placement = false
-
     until valid_placement
       coordinates = generate_random_coordinates(submarine.length)
       valid_placement = board.valid_placement?(submarine,coordinates)
     end
     # binding.pry
-
-    board.place(submarine, coordinates)
+    computer_board.place(submarine, coordinates)
+    computer_ships << submarine
+    @pcomputer_board.display
   end
-
   def generate_random_coordinates(ship_length)
-    
     rows = ["A", "B", "C", "D"]
     columns = ["1", "2", "3", "4"]
-
     random_row = rows.sample
     random_column = columns.sample
-
     initial_coordinate = random_row + random_column
-
     coordinates = [initial_coordinate]
-
     (ship_length -1).times do
       if rand(2) == 0
-        next_row = rows.index(random_row) + rand(ship_length)  
-        next_row = [next_row, rows.length - 1].min  
+        next_row = rows.index(random_row) + rand(ship_length)
+        next_row = [next_row, rows.length - 1].min
         coordinates << rows[next_row] + random_column
       else
-        next_column = columns.index(random_column) + rand(ship_length) 
-        next_column = [next_column, columns.length - 1].min  
+        next_column = columns.index(random_column) + rand(ship_length)
+        next_column = [next_column, columns.length - 1].min
         coordinates << random_row + columns[next_column]
       end
     end
     coordinates
-    
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,24 +1,32 @@
 require 'pry'
 require 'optparse'
-
 class Game
-  attr_reader
-
-  def initialize
+  attr_reader :player_board, :computer_board, :player_ships, :computer_ships
+   def initialize
+    @player_board = Board.new
+    @computer_board = Board.new
     @quit = false
-  end
-
-  def start_game
+    @game_over = false
+   end
+   def start_game
+    loop do
+      @board = Board.new
+      @player = Player.new
+      @computer = ComputerPlayer.new
+      @game_over = false
+      if game_over?
+        display_winner
+        break unless play_again?
+      end
+    end
     puts "Welcome to BATTLESHIP!"
     puts "Enter 'p' to play. Enter 'q' to quit."
-
     while !@quit
       user_input = gets.chomp.downcase
-
       case user_input
       when 'p'
         puts "Starting a new game..."
-        setup_new_game
+        start_new_game
       when 'q'
         puts "Goodbye"
         @quit = true
@@ -27,9 +35,64 @@ class Game
       end
     end
   end
-
-  def setup_new_game
+  def play_again?
+    puts "Do you want to play again? (yes/no)"
+    response = gets.chomp.downcase
+    response == "yes"
+  end
+   def start_new_game
     computer.place_ships(ship, coordinates)
     player.place_ships(ship, coordinates)
+   end
+   def play_turns
+    display_boards
+    player_shot
+    computer_shot unless game_over?
+  end
+  def display_boards
+    puts "============= COMPUTER BOARD ============="
+    computer_board.display(reveal_ships: false)
+    puts "============== PLAYER BOARD ==============="
+    player_board.display
+  end
+  def player_shot
+    puts "Enter the coordinate for your shot:"
+    coordinate = gets.chomp.upcase
+    until valid_shot?(coordinate)
+      puts "Please enter a valid coordinate:"
+      coordinate = gets.chomp.upcase
+    end
+    result = computer_board.fire_upon(coordinate)
+    display_shot_result(coordinate, result, "Your")
+  end
+  def computer_shot
+    computer = ComputerPlayer.new
+    coordinate = computer.generate_computer_shot(player_board)
+    result = player_board.fire_upon(coordinate)
+    display_shot_result(coordinate, result, "My")
+  end
+  def valid_shot?(coordinate)
+    player_board.valid_coordinate?(coordinate) && !player_board.cell_fired_upon?(coordinate)
+  end
+  def display_shot_result(coordinate, result, player)
+    case result
+    when :miss
+      puts "Player shot on #{coordinate} was a miss."
+    when :hit
+      puts "Player shot on #{coordinate} was a hit!"
+    when :sunk
+      puts "Player shot on #{coordinate} sunk a ship!"
+    end
+  end
+  def game_over?
+    player_ships.all?(&:sunk?) || computer_board.all_ships_sunk?
+  end
+  def display_winner
+    if @player_ships.all?(&:sunk?)
+      puts "I won! Better luck next time."
+    else
+      puts "Congratulations! You won Battleship!"
+    end
   end
 end
+BattleshipGame.new.start_game
