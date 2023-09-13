28a02f5ac551430e3fe81e8fc048de182c28f112
Heather McNeil <hd_mcneil@yahoo.com>
2023-09-13 10:32:49 -0700

    Initialize game on game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 RSpec.describe Game do
   before :each do
+    @game = Game.new
+    @computer_player = ComputerPlayer.new
+    @player = Player.new
+  end
 
+  it 'initializes the game state correctly' do
+    
   end
 end
